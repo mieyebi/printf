32bb90730e729c9331648f2dca6a7e00f8416979
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-20 22:35:54 +0000

    update _printf function

--- _printf.c
+++ _printf.c
@@ -39,6 +39,11 @@ int _printf(const char *format, ...)
 				}
 				else if (format[i + 1] == 0)
 					return (-1);
+				else
+				{
+					count += p_char(format[i]);
+					i++;
+				}
 			}
 		}
 		else
